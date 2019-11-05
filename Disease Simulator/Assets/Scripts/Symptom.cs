54d031fb9eea9416c965be335fad04daa133b6d6
ScienceMan314 <57101247+ScienceMan314@users.noreply.github.com>
2019-11-05 17:48:37 -0500

    Creates the Symptom Class

---
+++ Disease Simulator/Assets/Scripts/Symptom.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Symptom : MonoBehaviour
+{
+    private string _name;
+    private float _infectivityScore;
+    private float _severityScore;
+
+    public string Name {
+        get => _name;
+        set {
+            if(IsNullOrEmpty(value))
+                throw new NullReferenceException();
+            else
+                _name = value;
+        }
+    }
+    public float InfectivityScore {
+        get => _infectivityScore;
+        set {
+            if(0 <= value <= 1)
+                _infectivityScore = value;
+            else
+                throw new ArgumentException();
+        }
+    }
+
+    public float SeverityScore {
+        get => _severityScore;
+        set {
+            if(0 <= value <= 1)
+                _severityScore = value;
+            else
+                throw new ArgumentException();
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start(string n, float i, float s)
+    {
+        Name = n;
+        InfectivityScore = i;
+        SeverityScore = s;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
