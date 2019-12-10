5d67fdd53310e6e7dda0bf6e6240e193b9bc8d1f
Duppster <lqvalproductions@gmail.com>
2019-12-09 22:15:31 -0500

    Citizen Scheduling and Building Sizes
    Numerous bug fixes regarding building sizes
    
    - Added doors to every building
    - Building doors do not stretch to top of the building
    - Fixed navmesh issue regarding targets
    - Added person targeting methods.
    - Added clock system.

--- Disease Simulator/Assets/Scripts/PersonHandler.cs
+++ Disease Simulator/Assets/Scripts/PersonHandler.cs
@@ -6,6 +6,7 @@ public class PersonHandler : MonoBehaviour
 {
     public GameObject person;
     public int personcount = 10;
+    public float time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +17,25 @@ public class PersonHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
     }
 
     void createPerson(){
         List<GameObject> houses = GameObject.Find("CityGenerator").GetComponent<CityGenerator>().getHouses();
         if(houses.Count != 0){
             GameObject house = houses[(int) Random.Range(0,houses.Count)];
-            person = Instantiate(person, new Vector3(house.transform.position.x,house.transform.position.y+10F,house.transform.position.z),Quaternion.identity);
+            person = Instantiate(person, new Vector3(house.transform.GetChild(1).position.x+3,house.transform.position.y+10F,house.transform.position.z),Quaternion.identity);
             person.transform.parent = GameObject.Find("People").transform;
+            person.GetComponent<Person>().setTarget(house.transform.GetChild(1));
         }
         
+    }
+
+    void updateSchedules(){
+
+    }
+
+    float getTime(){
+        return time;
     }
 }
